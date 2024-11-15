206133042348997102c59f3ef9374aa4e7df9af6
kennyg37 <k.ganza@alustudent.com>
2024-11-15 10:13:25 +0200

    configuring ports and fixing issues

--- app.py
+++ app.py
@@ -50,7 +50,6 @@ def display_page(pathname):
 
 register_callbacks(app)
 
-port = os.getenv("PORT", 5000)
-
 if __name__ == "__main__":
-    app.run(debug=True, port=port)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run_server(host="0.0.0.0", port=port)
